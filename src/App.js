556dfac930a99311380b7d89d0c16fee45c95fee
talhazafarsb <82565002+talhazafarsb@users.noreply.github.com>
2021-07-19 20:59:04 +0500

    complete todo-list

--- src/App.js
+++ src/App.js
@@ -10,10 +10,10 @@ function App() {
   return (
     <div className="App">
         <header>
-            <h1>ToDo List {inputText}</h1>
+            <h1>ToDo List</h1>
         </header>
-        <Form setInputText = {setInputText}/>
-        <TodoList />
+        <Form todos = {todos} setTodos = {setTodos} setInputText = {setInputText} inputText={inputText}/>
+        <TodoList todos = {todos} setTodos = {setTodos}/>
     </div>
   );
 }
